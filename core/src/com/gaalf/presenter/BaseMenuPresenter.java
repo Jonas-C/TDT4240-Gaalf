5dddb6613e29105e5a73a21af71bc6c22dd77dd8
Jonas-C <jonas.c@hotmail.no>
2020-04-08 13:27:48 +0200

    Moved cloud movement to a Manager class, which is a singleton

--- core/src/com/gaalf/presenter/BaseMenuPresenter.java
+++ core/src/com/gaalf/presenter/BaseMenuPresenter.java
@@ -3,37 +3,18 @@ package com.gaalf.presenter;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.utils.Array;
 import com.gaalf.GaalfGame;
+import com.gaalf.manager.CloudsManager;
 import com.gaalf.view.BaseMenuView;
-
-import java.util.Random;
 
 public abstract class BaseMenuPresenter extends BasePresenter {
 
-    public static final int MAX_CLOUD_Y = 700;
-    public static final int MIN_CLOUD_Y = 300;
-    public static final int MAX_CLOUD_X = GaalfGame.V_WIDTH;
-    public static final int MIN_CLOUD_X = 0;
     Music menuMusic;
-    Array<Sprite> clouds;
-    float[] cloudSpeeds;
-    Random rand;
+    private CloudsManager cloudsManager;
 
     BaseMenuPresenter(GaalfGame game) {
         super(game);
-        TextureAtlas cloudsAtlas = game.assetManager.manager.get(game.assetManager.cloudsAtlas);
-        rand = new Random();
-        clouds = new Array<>();
-        clouds.addAll(cloudsAtlas.createSprites());
-        cloudSpeeds = new float[clouds.size];
-        for(int i = 0; i < clouds.size; i++){
-            clouds.get(i).setPosition(rand.nextInt((MAX_CLOUD_X - MIN_CLOUD_X) + 1) + MIN_CLOUD_X,
-                    rand.nextInt((MAX_CLOUD_Y - MIN_CLOUD_Y) + 1) + MIN_CLOUD_Y);
-            cloudSpeeds[i] = (rand.nextInt((10 - 2) + 1) + 2) / 10f;
-
-        }
+        cloudsManager = CloudsManager.getInstance(game.assetManager);
 
         menuMusic = game.assetManager.manager.get(game.assetManager.menuMusic);
         menuMusic.setLooping(true);
@@ -48,12 +29,7 @@ public abstract class BaseMenuPresenter extends BasePresenter {
 
     @Override
     public void update(float delta){
-        for(int i = 0; i < clouds.size; i++){
-            clouds.get(i).setX(clouds.get(i).getX() + cloudSpeeds[i]);
-            if(clouds.get(i).getX() > MAX_CLOUD_X){
-                clouds.get(i).setX(-clouds.get(i).getRegionWidth());
-            }
-        }
-        getView().drawBackground(new Sprite((Texture)game.assetManager.manager.get(game.assetManager.background)), clouds);
+        cloudsManager.updateClouds();
+        getView().drawBackground(new Sprite((Texture)game.assetManager.manager.get(game.assetManager.background)), cloudsManager.getClouds());
     }
 }
--- core/src/com/gaalf/view/BaseMenuView.java
+++ core/src/com/gaalf/view/BaseMenuView.java
@@ -1,11 +1,8 @@
 package com.gaalf.view;
 
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.EventListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Slider;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.utils.Array;
