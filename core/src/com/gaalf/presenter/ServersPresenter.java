ac5f29bd8ea93cee718321783191162f7d976a93
Trym Vegard Gjelseth-Borgen <tvgb@outlook.com>
2020-04-08 22:18:29 +0200

    Work in progress

--- core/src/com/gaalf/presenter/ServersPresenter.java
+++ core/src/com/gaalf/presenter/ServersPresenter.java
@@ -1,18 +1,51 @@
 package com.gaalf.presenter;
 
 import com.gaalf.GaalfGame;
+import com.gaalf.network.GameServerSpecification;
+import com.gaalf.network.MatchmakingClient;
 import com.gaalf.view.BaseView;
 import com.gaalf.view.ServersView;
 
+import java.io.IOException;
+import java.util.List;
+
 public class ServersPresenter extends BaseMenuPresenter {
     private BaseView view;
+    private MatchmakingClient matchmakingClient;
 
     public ServersPresenter(final GaalfGame game){
         super(game);
         view = new ServersView(game.getBatch(), this);
+        matchmakingClient = createMatchmakingClient();
+        System.out.println(matchmakingClient);
     }
+
+    public void openMainMenuView() {
+        game.setScreen(new MainMenuPresenter(game));
+    }
+
     @Override
     public BaseView getView() {
         return view;
+    }
+
+    public List<GameServerSpecification> getGameServers() {
+        if (matchmakingClient != null) {
+            try {
+                return matchmakingClient.getGameServers();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        return null;
+    }
+
+    private MatchmakingClient createMatchmakingClient() {
+        try {
+           return new MatchmakingClient();
+        } catch (IOException e) {
+            return null;
+        }
     }
 }
--- core/src/com/gaalf/view/ServersView.java
+++ core/src/com/gaalf/view/ServersView.java
@@ -1,16 +1,54 @@
 package com.gaalf.view;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.gaalf.network.GameServerSpecification;
 import com.gaalf.presenter.ServersPresenter;
+
+import java.util.List;
 
 public class ServersView extends BaseMenuView {
 
-    public ServersView(SpriteBatch batch, ServersPresenter presenter){
+    private final String TAG = MainMenuView.class.getSimpleName();
+
+    public ServersView(SpriteBatch batch, final ServersPresenter presenter){
         super(batch, presenter);
+
+        addTitle("Select server??");
+        table.row();
+
+        CreateServerSelectMenu();
+
+        TextButton backButton = addBackButton();
+        backButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                presenter.openMainMenuView();
+            }
+        });
+
+        addActor(table);
     }
 
     @Override
     public void update(float delta) {
 
+    }
+
+    public void CreateServerSelectMenu() {
+        List<GameServerSpecification> gameServers = ((ServersPresenter)getPresenter()).getGameServers();
+
+        if (gameServers != null) {
+            for (GameServerSpecification gameServer : gameServers) {
+                log(TAG, "Host: " + gameServer.host + ", players: " + gameServer.players);
+            }
+        } else {
+            Label label = new Label("No available servers ...", getSkin());
+            table.add(label);
+        }
     }
 }
